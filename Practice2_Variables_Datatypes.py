a06230b576a7467a453455e7c7d8a76197a3ab93
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-03-09 23:01:48 +0600

    Update Practice2_Variables_Datatypes.py

---
+++ Practice2_Variables_Datatypes.py
@@ -0,0 +1,26 @@
+# 1. Add two number
+a = 5
+b = 4
+print(a+b)
+
+# 2. Find remainder
+print(a % b)
+
+# 3. check input type 
+a = input("Enter any thing : " )
+print(type(a))
+
+# 4. check bigger number 
+a = 35
+b = 80
+c = a > b
+print(c)
+
+# 5. find avg of 2 number 
+d = (a+b)/2
+print(d)
+
+# 6. squre of a number enter by user 
+a = int(input("Enter any number : "))
+b = a*a
+print(b)
