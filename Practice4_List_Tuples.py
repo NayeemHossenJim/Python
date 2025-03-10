1ffa60d6992ecf9fc975f9e18c268db29d81703c
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-03-10 21:27:11 +0600

    update list tuples

--- Practice4_List_Tuples.py
+++ Practice4_List_Tuples.py
@@ -1,18 +1,23 @@
 # 1. User Inout Lists 
-fruits = []
-f1 = input("Enter fruit name : ")
-fruits.append(f1)
-f2 = input("Enter fruit name : ")
-fruits.append(f2)
-f3 = input("Enter fruit name : ")
-fruits.append(f3)
-f4 = input("Enter fruit name : ")
-fruits.append(f4)
-f5 = input("Enter fruit name : ")
-fruits.append(f5)
-f6 = input("Enter fruit name : ")
-fruits.append(f6)
-f7 = input("Enter fruit name : ")
-fruits.append(f7)
+# fruits = []
+# f1 = input("Enter fruit name : ")
+# fruits.append(f1)
+# f2 = input("Enter fruit name : ")
+# fruits.append(f2)
+# f3 = input("Enter fruit name : ")
+# fruits.append(f3)
+# f4 = input("Enter fruit name : ")
+# fruits.append(f4)
+# f5 = input("Enter fruit name : ")
+# fruits.append(f5)
+# f6 = input("Enter fruit name : ")
+# fruits.append(f6)
+# f7 = input("Enter fruit name : ")
+# fruits.append(f7)
 
-print(fruits)
+# print(fruits)
+
+# 2. Sorted Number
+number = [1,8,2,4,5,6]
+number.sort()
+print(number)
