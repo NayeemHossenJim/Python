e89da310f4438b2898ed0809bd5ec381b4bae4f0
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-03-10 06:53:12 +0600

    Update Practice3_String.py

--- Practice3_String.py
+++ Practice3_String.py
@@ -1,3 +1,18 @@
 # 1. User input name 
 a = input()
-print("Good Morning " + a)
+print(f"Good Morning ,{a}")
+
+# 2. Letter template 
+letter = '''Dear Name
+You r selected 
+Date'''
+x = letter.replace("Name",a).replace("Date","24 September")
+print(x)
+
+# 3. Find Method 
+j = x.find("Jim")
+print(j)
+
+# 4. Backslash character 
+h = '''Hello \nThere are \tthree cow'''
+print(h)
