8229916efd85b3b7aba2f15399fb3feca548760c
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-03-14 07:10:04 +0600

    Update Practice7_Loop.py

--- Practice7_Loop.py
+++ Practice7_Loop.py
@@ -1,14 +1,14 @@
 # 1. Table 
-# number = int(input("Enter your number: "))
-# for i in range(1,11):
-#     print(f"{number}*{i} =",number * i)
-#     i += 1
+number = int(input("Enter your number: "))
+for i in range(1,11):
+    print(f"{number}*{i} =",number * i)
+    i += 1
 
 # 2. Greet Name start with s 
-# L = ("Harry","Shuvam","Shanya","Raju")
-# for i in L:
-#     if(i.startswith("S")):
-#         print(f"Hello {i}")
+L = ("Harry","Shuvam","Shanya","Raju")
+for i in L:
+    if(i.startswith("S")):
+        print(f"Hello {i}")
 
 # 3. Table using while loop 
 number = int(input("Enter your number: "))
